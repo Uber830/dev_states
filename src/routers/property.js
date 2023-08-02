b38eaddf959e3f32c5c3bce9317c5f41a02ed9bc
Uber Cardoso <hhcardoso@misena.edu.co>
2023-08-02 15:37:34 -0500

    router and controller for property

--- src/routers/property.js
+++ src/routers/property.js
@@ -6,17 +6,19 @@ import {
     getIdProperty,
     updateProperty,
 } from "../controllers/property.js";
+//import { checkJwt } from "../middlewares/securityRole.js";
+import { verifyAuth } from "../middlewares/authValidate.js";
 
 const routerPro = Router();
 
-routerPro.get("/all", getAllProperty);
+routerPro.get("/all", verifyAuth, getAllProperty);
 
-routerPro.get("/id", getIdProperty);
+routerPro.get("/id", verifyAuth, getIdProperty);
 
-routerPro.post("/create/id", createProperty);
+routerPro.post("/create/id", verifyAuth, createProperty);
 
-routerPro.patch("/update/id", updateProperty);
+routerPro.patch("/update/id", verifyAuth, updateProperty);
 
-routerPro.delete("/delete/id", deleteProperty);
+routerPro.delete("/delete/id", verifyAuth, deleteProperty);
 
 export { routerPro };
