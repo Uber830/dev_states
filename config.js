951717d7366e29b9cc2b0a7dfdb8b828fa91caa9
Uber Cardoso <hhcardoso@misena.edu.co>
2023-08-03 01:58:40 -0500

    resolve error connection

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ export const PORT_SERVER = process.env.PORT_SERVER || 3010;
 const DB_PORT = process.env.DB_PORT || 3306;
 const DB_NAME = process.env.DB_NAME || "name";
 const DB_USER = process.env.DB_USER || "user";
-const DB_HOST = process.env.TZ_HOST || "localhost";
+const DB_HOST = process.env.DB_HOST || "localhost";
 const DB_PASSWORD = process.env.DB_PASSWORD || "password";
 
 export {
