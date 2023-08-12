376ae9bb0afd2cd368a91369f40e42710e8837ae
Uber Cardoso <hhcardoso@misena.edu.co>
2023-08-12 02:15:45 -0500

    update router add middleware for validate

--- src/routers/property.js
+++ src/routers/property.js
@@ -1,24 +1,19 @@
 import { Router } from "express";
-import {
-    createProperty,
-    deleteProperty,
-    getAllProperty,
-    getIdProperty,
-    updateProperty,
-} from "../controllers/property.js";
-//import { checkJwt } from "../middlewares/securityRole.js";
+import {createProperty,deleteProperty,getAllProperty,getIdProperty,updateProperty} from "../controllers/property.js";
 import { verifyAuth } from "../middlewares/authValidate.js";
+import { propertyValidate } from "../DTO/propertyValidate.js";
+import { authRole } from "../middlewares/authRole.js";
 
 const routerPro = Router();
 
 routerPro.get("/all", verifyAuth, getAllProperty);
 
-routerPro.get("/id", verifyAuth, getIdProperty);
+routerPro.get("/:id", verifyAuth, getIdProperty);
 
-routerPro.post("/create/id", verifyAuth, createProperty);
+routerPro.post("/create", verifyAuth, authRole(["vendor"]), propertyValidate, createProperty);
 
-routerPro.patch("/update/id", verifyAuth, updateProperty);
+routerPro.patch("/update/:id", verifyAuth, authRole(["vendor"]), propertyValidate, updateProperty);
 
-routerPro.delete("/delete/id", verifyAuth, deleteProperty);
+routerPro.delete("/delete/:id", verifyAuth, authRole(["admin","vendor"]),deleteProperty);
 
 export { routerPro };
