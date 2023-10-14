60457210aae02640706b5d35ea7c890736476226
Uber Cardoso <hhcardoso@misena.edu.co>
2023-10-14 00:43:35 -0500

    crud of property functional

--- src/controllers/property.js
+++ src/controllers/property.js
@@ -1,9 +1,12 @@
 import { poll } from "../../database/dbmysql.js";
+import { uploadImage, deleteImage } from "../helpers/cloudinary.js";
+import fs from "fs-extra";
+import { deleteImageCloud } from "../helpers/deleteImageCloud.js";
 
 // get all properties
 const getAllProperty = async (req, res) => {
   try {
-    const [data] = await poll.query("SELECT * FROM property ORDER BY type");
+    const [data] = await poll.query("SELECT * FROM property ORDER BY id");
 
     if (data.length < 1) {
       throw new Error(`Property is empty ${res.status(404)}`);
@@ -39,16 +42,23 @@ const getIdProperty = async (req, res) => {
 const createProperty = async (req, res) => {
   const { type, address, area, price, characteristic, id_states } = req?.body;
 
-  // create DTO for the property, validate values
+  // upload image to cloudinary
+  const cloudImage = await uploadImage(req?.files?.image_url?.tempFilePath);
+  const { public_id, url } = cloudImage;
+
   try {
     const [rows] = await poll.query(
-      "INSERT INTO property(type, address, area, price, characteristic, id_states) VALUES(?,?,?,?,?,?)",
-      [type, address, area, price, characteristic, id_states]
+      `INSERT INTO property(type,id_image,image_url,address,area,price,characteristic,id_states) 
+      VALUES(?,?,?,?,?,?,?,?)`,
+      [type, public_id, url, address, area, price, characteristic, id_states]
     );
 
     if (rows.affectedRows !== 1) {
       throw new Error("Invalid values for property");
     }
+
+    // delete image in folder local 'uploads'
+    await fs.unlink(req?.files?.image_url?.tempFilePath);
 
     res.status(201).json({ property: "Created property successfully" });
   } catch (err) {
@@ -60,16 +70,25 @@ const createProperty = async (req, res) => {
 const updateProperty = async (req, res) => {
   const { id } = req?.params;
   const { type, address, area, price, characteristic, id_states } = req?.body;
-  
+
+  // upload image to cloudinary
+  const cloudImage = await uploadImage(req?.files?.image_url.tempFilePath);
+  const { public_id, url } = cloudImage;
+
   try {
     const [rows] = await poll.query(
-      `UPDATE property SET type = IFNULL(?,type), address = IFNULL(?,address), area = IFNULL(?,area), price = IFNULL(?,price),characteristic = IFNULL(?,characteristic), id_states = IFNULL(?,id_states) WHERE id = (?)`,
-      [type, address, area, price, characteristic, id_states, id]
+      `UPDATE property SET type = IFNULL(?,type), id_image = IFNULL(?, id_image), image_url = IFNULL(?, image_url), 
+      address = IFNULL(?,address), area = IFNULL(?,area), price = IFNULL(?,price),
+      characteristic = IFNULL(?,characteristic), id_states = IFNULL(?,id_states) WHERE id = (?)`,
+      [type, public_id, url, address, area, price, characteristic, id_states, id]
     );
 
     if (rows.affectedRows !== 1) {
       throw new Error("Invalid query parameters for update");
     }
+
+    // delete image in folder local 'uploads'
+    await fs.unlink(req?.files?.image_url?.tempFilePath);
 
     res.status(201).json({ property: "Update property successfully" });
   } catch (err) {
@@ -82,6 +101,15 @@ const deleteProperty = async (req, res) => {
   const { id } = req.params;
 
   try {
+    // search id_image in database
+    const [data] = await poll.query(
+      "SELECT id_image FROM property WHERE id = (?)",
+      id
+    );
+
+    // shearch id_image in db and delete in cloudinary
+    await deleteImageCloud(data);
+
     const [rows] = await poll.query("DELETE FROM property WHERE id = (?)", id);
 
     if (rows.affectedRows < 1) {
@@ -90,7 +118,7 @@ const deleteProperty = async (req, res) => {
 
     res.sendStatus(204);
   } catch (err) {
-    return res.status(err.statusCode()).json({ error: err.menssage });
+    return res.status(500).json({ error: err.menssage });
   }
 };
 
--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,11 +1,13 @@
 import { poll } from "../../database/dbmysql.js";
+import fs from "fs-extra";
 import { hashPassword } from "../helpers/verifypassword.js";
+import { uploadImage, deleteImage } from "../helpers/cloudinary.js";
 
 // Search all users in the database
 const getAllUsers = async (req, res) => {
   try {
     const [data] = await poll.query(
-      "SELECT id,cc,firtsname,lastname,image_profile,email,id_role,state,date_create,date_update FROM users"
+      "SELECT id,cc,firtsname,lastname,image_profile,email,id_role,date_create,date_update FROM users"
     ); // not returning a password
 
     if (data.length <= 0)
@@ -23,7 +25,7 @@ const getIdUsers = async (req, res) => {
 
   try {
     const [data] = await poll.query(
-      "SELECT cc,firtsname,lastname,image_profile,email,id_role,state,date_create,date_update FROM users WHERE id = (?) AND state = 'activo'",
+      "SELECT cc,firtsname,lastname,image_profile,email,id_role,date_create,date_update FROM users WHERE id = (?) AND state = 'activo'",
       Number(id)
     );
 
@@ -39,14 +41,14 @@ const getIdUsers = async (req, res) => {
 
 // Create a new user
 const createUsers = async (req, res) => {
-  const { cc, firtsname, lastname, email, password ,id_role } = req?.body;
+  const { firtsname, email, password, id_role } = req?.body;
 
   try {
     const hash = await hashPassword(password); // generate hash
 
     const [rows] = await poll.query(
-      "INSERT INTO users (cc,firtsname,lastname,email,password,id_role) VALUES (?,?,?,?,?,?)",
-      [cc, firtsname, lastname, email, hash, id_role]
+      "INSERT INTO users (firtsname,email,password,id_role) VALUES (?,?,?,?)",
+      [firtsname, email, hash, id_role]
     );
 
     if (rows.affectedRows != 1) {
@@ -62,20 +64,28 @@ const createUsers = async (req, res) => {
 // Update user profile
 const updateUsers = async (req, res) => {
   const { id } = req?.params;
-  const { cc, firtsname, lastname, image_profile, email, password } = req?.body;
+  const { cc, firtsname, lastname, email, password } = req?.body;
+
+  // upload image in cloudinary
+  const cloudImage = await uploadImage(req?.files?.image_profile?.tempFilePath);
+  const { public_id, url } = cloudImage;
 
   const hash = await hashPassword(password); // generate hash
 
   try {
     const [rows] = await poll.query(
       `UPDATE users SET cc = IFNULL(?,cc), firtsname = IFNULL(?,firtsname), lastname = IFNULL(?,lastname), 
-      image_profile = IFNULL(?,image_profile) ,email = IFNULL(?,email), password = IFNULL(?,password) WHERE id = (?)`,
-      [cc, firtsname, lastname, image_profile, email, hash, id]
+      id_image = IFNULL(?,id_image), image_profile = IFNULL(?,image_profile) ,email = IFNULL(?,email), 
+      password = IFNULL(?,password) WHERE id = (?)`,
+      [cc, firtsname, lastname, public_id, url, email, hash, id]
     );
 
     if (rows.affectedRows !== 1) {
       throw new Error("Error while trying to update the user");
     }
+
+    // delete image in folder local 'uploads'
+    await fs.unlink(req?.files?.image_profile?.tempFilePath);
 
     res.status(202).json({ result: "user update successfully" });
   } catch (err) {
@@ -86,15 +96,31 @@ const updateUsers = async (req, res) => {
 // Delete user in database
 const deleteUsers = async (req, res) => {
   const { id } = req?.params;
-
   try {
-    const [rows] = await poll.query(
-      "UPDATE users SET state = 'eliminado' WHERE id = (?)",
+    // search id_image in database
+    const [data] = await poll.query(
+      "SELECT id_image FROM users WHERE id = (?)",
       id
     );
 
+    // validate if the user has an image
+    const id_image = data?.[0]?.id_image;
+
+    if (typeof id_image !== "object") {
+      // delete image in cloudinary
+      await deleteImage(id_image);
+    }
+  } catch (err) {
+    res.status(400).send({ message: err.message });
+  }
+
+  try {
+    // delete user in database by id
+    const [rows] = await poll.query("DELETE FROM users WHERE id = (?)", id);
+
+    // validate if the user was deleted
     if (rows?.affectedRows !== 1) {
-      throw new Error("User not deleted, exist warnings");
+      throw new Error(`Error while trying to delete the user with id ${id}`);
     }
 
     res.sendStatus(204);
