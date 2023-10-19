dd71bd70ddb7da333e8cf6d25081baf0f9b486dc
Uber Cardoso <hhcardoso@misena.edu.co>
2023-10-18 23:11:11 -0500

    problem solving with merge, basic configurations

--- config.js
+++ config.js
@@ -7,10 +7,17 @@ const DB_USER = process.env.DB_USER || "user";
 const DB_HOST = process.env.DB_HOST || "localhost";
 const DB_PASSWORD = process.env.DB_PASSWORD || "password";
 
+const CLOUDINARY_NAME = process.env.CLOUDINARY_NAME || "";
+const CLOUDINARY_KEY = process.env.CLOUDINARY_KEY || "";
+const CLOUDINARY_SECRET = process.env.CLOUDINARY_SECRET || "";
+
 export {
   DB_PORT,
   DB_NAME,
   DB_HOST,
   DB_PASSWORD,
   DB_USER,
+  CLOUDINARY_NAME,
+  CLOUDINARY_KEY,
+  CLOUDINARY_SECRET,
 };
